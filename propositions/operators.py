969df5c84d0f85dbce0d84d6b8c1fa479b4f77a1
sofya <msvvm06@gmail.com>
2026-02-18 20:04:20 +0300

    Update operators.py

--- propositions/operators.py
+++ propositions/operators.py
@@ -23,6 +23,41 @@ def to_not_and_or(formula: Formula) -> Formula:
         ``'|'``.
     """
     # Task 3.5
+    root = formula.root
+    if is_variable(root):
+        return formula
+    if root == 'T':
+        p = Formula('p')
+        return Formula('|',p, Formula('~', p))
+    if root =='F':
+        p = Formula('p')
+        return Formula('&',p, Formula('~', p))
+    if root == '~':
+        inner = to_not_and_or(formula.first)
+        return Formula('~', inner)
+    left = to_not_and_or(formula.first)
+    right = to_not_and_or(formula.second)
+    if root == '&':
+        return Formula('&',left, right)
+    if root == '|':
+        return Formula('|',left, right)
+    if root == '->':
+        return Formula('|',Formula('~', left),right)
+
+    if root =='+':
+        a = Formula('&', left, Formula('~', right))
+        b =Formula('&', Formula('~', left), right)
+        return Formula('|', a,b)
+
+    if root == '<->':
+        a= Formula('&',left, right)
+        b = Formula('&',Formula('~', left),Formula('~', right))
+        return Formula('|', a, b)
+    if root== '-&':
+        return Formula('~', Formula('&',left, right))
+    if root =='-|':
+        return Formula('~',Formula('|', left, right))
+    assert False
 
 def to_not_and(formula: Formula) -> Formula:
     """Syntactically converts the given formula to an equivalent formula that
@@ -36,7 +71,10 @@ def to_not_and(formula: Formula) -> Formula:
         contains no constants or operators beyond ``'~'`` and ``'&'``.
     """
     # Task 3.6a
-
+    temp = to_not_and_or(formula)
+    new_formula = temp.substitute_operators({
+        '|': Formula.parse('~(~p&~q)')})
+    return new_formula
 def to_nand(formula: Formula) -> Formula:
     """Syntactically converts the given formula to an equivalent formula that
     contains no constants or operators beyond ``'-&'``.
@@ -49,6 +87,9 @@ def to_nand(formula: Formula) -> Formula:
         contains no constants or operators beyond ``'-&'``.
     """
     # Task 3.6b
+    f = to_not_and(formula)
+    f = f.substitute_operators({'~': Formula.parse('(p-&p)'), '&': Formula.parse('((p-&q)-&(p-&q))')})
+    return f
 
 def to_implies_not(formula: Formula) -> Formula:
     """Syntactically converts the given formula to an equivalent formula that
@@ -62,6 +103,9 @@ def to_implies_not(formula: Formula) -> Formula:
         contains no constants or operators beyond ``'->'`` and ``'~'``.
     """
     # Task 3.6c
+    f = to_not_and_or(formula)
+    f = f.substitute_operators({'|': Formula.parse('(~p->q)'),'&': Formula.parse('~(p->~q)')})
+    return f
 
 def to_implies_false(formula: Formula) -> Formula:
     """Syntactically converts the given formula to an equivalent formula that
@@ -75,3 +119,7 @@ def to_implies_false(formula: Formula) -> Formula:
         contains no constants or operators beyond ``'->'`` and ``'F'``.
     """
     # Task 3.6d
+    temp = to_implies_not(formula)
+    new_form= temp.substitute_operators({'~': Formula.parse('(p->F)')})
+    return new_form
+
